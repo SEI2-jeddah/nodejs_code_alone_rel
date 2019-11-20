d4bebfe7fecb0d423e8a026d28058240c7698fce
guru-schnecke <eiweala@live.com>
2019-11-20 12:53:44 +0300

    check point 1

--- helper/passport.js
+++ helper/passport.js
@@ -2,20 +2,41 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 
 passport.use(
-  new LocalStrategy(function(username, password, done) {
-    User.findOne({ username: username }, function(err, user) {
-      if (err) {
-        return done(err);
-      }
-      if (!user) {
-        return done(null, false, { message: "Incorrect username." });
-      }
-      if (!user.validPassword(password)) {
-        return done(null, false, { message: "Incorrect password." });
-      }
-      return done(null, user);
-    });
-  })
+  new LocalStrategy(
+    {
+      usernameField: "email",
+      passwordField: "password",
+      session: false
+      // passReqToCallback: true
+    },
+    (email, password, done) => {
+      User.findOne(
+        {
+          email: email
+        },
+        function(err, user) {
+          if (err) {
+            return done(err);
+          }
+          if (!user) {
+            return done(null, false, { message: "User doesnt exist" });
+          }
+
+          user.verifyPassword(password, user.password, (err, match) => {
+            if (err) {
+              return done(null, false, { message: "somethings wrong" });
+            }
+
+            if (!match) {
+              return done(null, false, { message: "passwords dont match" });
+            }
+
+            return done(null, user);
+          });
+        }
+      );
+    }
+  )
 );
 
 passport.serializeUser(function(user, done) {
